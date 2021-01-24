d5c11441af8dc1665e4a68e108a8150d4e0ae604
Eva <yi.zheng1709@gmail.com>
2021-01-24 01:25:36 -0500

    commit tree 30436ed4b1c6b7c12d4620a618a0d5eafc46b9d7

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,39 @@
+require 'pry'
 class Waiter
+    attr_accessor :name, :yrs_experience
+    @@all = []
+
+    def initialize(name, yrs_experience)
+        @name = name
+        @yrs_experience = yrs_experience
+        @@all << self 
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def new_meal(customer, total, tip=0)
+        Meal.new(self, customer, total, tip)
+    end 
+
+    def meals 
+        Meal.all.select {|meal| meal.waiter == self}
+    end 
+
+    def best_tipper 
+        best_tipped_meal = meals.max {|meal_a, meal_b| meal_a.tip <=> meal_b.tip}
+        best_tipped_meal.customer 
+    end 
+
+    def most_frequent_customers 
+        number = 0
+        customer = nil
+        list_of_customers = meals.collect {|meal| meal.customer}
+        binding.pry
+    end 
+        #go through the meals array and collect
+        #an array of customers and count the customers
+
 
 end
